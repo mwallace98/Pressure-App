e61cf848daa28231ca884f3e066f52c5a7a279c6
mwallace98 <mattwallace140@gmail.com>
2025-06-15 17:32:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+
+  const apiKey = '7fc7cb7c2ea8155f7646dd416eb80f02'
+
+  const [weatherData,setWeatherData] = useState({})
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+    params: {
+      appid: apiKey,
+      lat:42.117039,
+      lon: -71.864723,
+      units: 'metric'
+    }
+  })
+  .then(res => {
+    console.log(res.data.main,'data')
+    setWeatherData(res.data.main)
+  })
+  .catch(err => {
+    console.log(err,'err')
+  })
+  },[])
+
+  console.log(weatherData,'weather data')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Info</h1>
+        {weatherData.temp ? (
+          <div className="weather-card">
+            <p><strong>Temperature:</strong> {weatherData.temp * 9/5 + 32}°F</p>
+            <p><strong>Pressure:</strong> {(weatherData.pressure / 33.89).toFixed(2)} inHg</p>
+            <p><strong>Humidity:</strong> {weatherData.humidity}%</p>
+          </div>
+        ) : (
+          <p>Loading Weather Data...</p>
+        )}
       </header>
     </div>
   );
