9df411cdcef55dd28438965c3279fce5b349e104
mwallace98 <mattwallace140@gmail.com>
2025-06-17 12:14:22 -0400

    css updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [lat,setLat] = useState('')
   const [long,setLong] = useState('')
   const [address,setAddress] = useState('')
+  const [inputAddress, setInputAddress] = useState('')
 
 
   const fetchWeather = (latValue,longValue) => {
@@ -36,53 +37,31 @@ const fetchAddress = (address) => {
     params: {address}
   })
   .then(res => {
-    console.log(res.data.results[0].geometry.location,'res.data')
+    console.log(res.data.results[0].address_components[0].short_name,'res.data')
     setLong(res.data.results[0].geometry.location.lng)
     setLat(res.data.results[0].geometry.location.lat)
     fetchWeather(lat,long)
+    setAddress(res.data.results[0].address_components[0].short_name)
   })
   .catch(err => {
     console.log(err)
   })
 }
 
-
- const locations = {
-     Oxford: { lat: 42.117039, long:-71.864723, label:'Oxford MA'},
-     Douglas: {lat: 42.052819, long: -71.739823, label:'Douglas MA'},
-     Greenfield: {lat: 42.587879, long:-72.600258, label: 'Greenfield MA' },
-     Conway: {lat: 43.978691,long:-71.126213,label: 'Conway NH'  },
-     Narragansett: {lat: 41.433060, long:-71.460182, label: 'Narragansett RI' },
-     Sandwich: {lat: 41.757721, long:-70.500137, label: 'Sandwich MA'}
-    }
-
-
-  const handleLocationClick = (locKey) => {
-    const loc = locations[locKey];
-    const latStr = loc.lat.toString();
-    const longStr = loc.long.toString()
-    setLat(latStr);
-    setLong(longStr);
-    fetchWeather(latStr,longStr)
-  };
-
   return (
     
     <div className="App">
 
       <header className="App-header">
-        <h1>Weather Info for {weatherData.name}</h1>
+        
+        <h1>
+          {address ? `Weather Info for ${address}` : 'Enter an Address to get Weather'  }
+        </h1>
       
-        <div className="location-buttons">
-          {Object.keys(locations).map(key => (
-            <button key={key} onClick={() => handleLocationClick(key)}>
-              {locations[key].label}
-            </button>
-          ))}
-        </div>
+
         <div className='Location-form'>
-          <Search address={address} setAddress={setAddress}/>
-          <button onClick={() => fetchAddress(address)}>Search</button>
+          <Search address={inputAddress} setAddress={setInputAddress}/>
+          <button onClick={() => fetchAddress(inputAddress)}>Search</button>
         </div>
         {weatherData.main ? (
           <div className="weather-card">
