70eb944bb4c2d909bd0f6e1f3dbce36dfb213491
mwallace98 <mattwallace140@gmail.com>
2025-06-24 20:47:53 -0400

    chart updated

--- src/Components/chart.js
+++ src/Components/chart.js
@@ -22,14 +22,15 @@ function Chart(props){
 				data={data}
 				margin={{ top: 5, right: 1, left: 20, bottom: 5 }}
 			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name"
+				<XAxis 
+					dataKey="name"
+					tick={{ fill: "#ffffff" }}
 					tickFormatter={(str) => {
 						const date = new Date(str);
 						return date.toLocaleDateString("en-US", { month: "short", day: "numeric", hour: 'numeric' });
 					} }
 					interval="preserveStartEnd" />
-				<YAxis domain={[975, 1025]} />
+				<YAxis domain={[975, 1025]}  tick={{ fill: "#ffffff" }}/>
 				<Tooltip />
 				<Line type="monotone" dataKey="pressure" stroke="#ff6347" strokeWidth={2} dot={false} />
 			</LineChart>
